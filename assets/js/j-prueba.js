3d93120958d30e629c581ba73d532803a697b345
Alejandra Medina <97324520+alemcollarte@users.noreply.github.com>
2022-01-08 05:15:51 -0300

    Add files via upload

---
+++ assets/js/j-prueba.js
@@ -0,0 +1,32 @@
+// Permite que las anclas desde el menu se deslicen suavemente y ademas mas abajo la section desplegar
+$(document).ready(function (){
+
+$("a").click(function () {
+var gato=this.hash;
+$("html, body").animate(
+    {
+scrollTop: $(gato).offset().top - 70
+
+    }, 3000
+)
+
+
+})
+
+
+
+})
+// Se agrega tooltiptrigger
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+/* uso de clase utilitaria con js script al hacer click a un tag*/
+$(document).ready(function (){
+
+  $("#cambia").click(function(){
+ $("#cambia").toggleClass("fs-4");
+    })
+
+  })
+
